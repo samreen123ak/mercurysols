ce225b5843723a76c225815ea779ad12d239f1e8
samreen123ak <sam.bhatti3399@gmail.com>
2024-01-05 22:23:58 +0500

    my first code

---
+++ src/Components/Mainpage.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import './mycss/mainpage.css';
+
+const Mainpage = () => {
+
+  return (
+    <>
+            
+        <div className='main1'>
+          <div className="side-by-side">
+          Transforming Ideas Into Software 
+            <div className="scroller">
+              <span>
+                Experiences<br/>
+                Innovation<br/>
+                Creation<br/>
+              </span>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+          <div className='mainbutton'>
+            <button className='button1'>About Us</button>
+            <button className='button2'>contact us</button>
+          </div>
+        </div>
+        <div className=''><img src="glob.png"/></div>
+      </div>
+
+      <div className='titlemain2'>Our Vison & Our Goal</div>
+      
+      <div className='main2'>
+     
+      <div className='main2-1'><img src="main2.png"/></div>
+      <div className='main2-2'> 
+      <div className="container">
+            <div className="box">
+                <img className="image" src="1.png" alt="Image 1" />
+                <div className="text">mobile app development</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+
+            <div className="box">
+                <img className="image" src="2.png" alt="Image 2" />
+                <div className="text">Website development</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+
+            <div className="box">
+                <img className="image" src="3.png" alt="Image 3" />
+                <div className="text">E-Commerce development</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+
+            <div className="box">
+                <img className="image" src="4.png" alt="Image 4" />
+                <div className="text">UI UX</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+
+            <div className="box">
+                <img className="image" src="5.png" alt="Image 5" />
+                <div className="text">Game development</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+
+            <div className="box">
+                <img className="image" src="6.png" alt="Image 6" />
+                <div className="text">IPTV development</div>
+                <div className="arrow">&#8594;</div>
+            </div>
+        </div></div>
+      </div>
+      <div className='main2-3'>
+        <div className='main2-3_title'>why choose us?</div>
+        <div className='main2-3_maindata'>
+        <div className='main2-3_maindata-1'>
+            <div className='main2-3_maindata-1-1'>
+              <div className='main2-3_maindata-1-1-image'><img src="11.png"/></div>
+              <div className='main2-3_maindata-1-1-data'><b>Innovation and Expertise</b> <br/>Your Elite IT Partner for Web Development, E-Commerce, and IPTV solutions.</div>
+              <div className='main2-3_maindata-1-1-image'></div>
+              <div><img src="12.png"/></div>
+              <div className='main2-3_maindata-1-1-data'><b>Innovation and Expertise</b> <br/>Your Elite IT Partner for Web Development, E-Commerce, and IPTV solutions.</div>
+            </div>
+            <div className='main2-3_maindata-1-1'>
+              <div className='main2-3_maindata-1-1-image'><img src="13.png"/></div>
+              <div className='main2-3_maindata-1-1-data'><b>Innovation and Expertise</b> <br/>Your Elite IT Partner for Web Development, E-Commerce, and IPTV solutions.</div>
+              <div className='main2-3_maindata-1-1-image'></div>
+              <div><img src="14.png"/></div>
+              <div className='main2-3_maindata-1-1-data'><b>Innovation and Expertise</b> <br/>Your Elite IT Partner for Web Development, E-Commerce , and IPTV solutions.</div>
+            </div>
+            <div className='main2-3_maindata-1-1'>
+              <div className='main2-3_maindata-1-1-image'><img src="15.png"/></div>
+              <div className='main2-3_maindata-1-1-data'><b>Innovation and Expertise</b> <br/>Your Elite IT Partner for Web Development, E-Commerce, and IPTV solutions.</div></div>
+              <div className='main2-3_maindata-1-1-image'></div>
+          </div>
+          <div className='main2-3_maindata-2'><img src="Subtract.png" width={"400px"}/>
+            <div className='image5'><img src="_ (1).png"/></div><div className='image6'><img src="_.png"/></div>
+          </div>
+        </div>
+        </div>
+    </>
+  );
+};
+
+export default Mainpage;
